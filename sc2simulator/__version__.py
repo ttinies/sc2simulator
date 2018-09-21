273d587c1df900cb5bee3d4ef26162eb0033d3de
ttinies <ttinies.v@gmail.com>
2018-09-20 21:35:12 -0600

    - bumped to highlight newly added feature: xml parsing

--- sc2simulator/__version__.py
+++ sc2simulator/__version__.py
@@ -17,6 +17,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = (0, 1, 0)
+VERSION = (0, 2, 0)
 
 __version__ = '.'.join(map(str, VERSION))
