8407e2745710ba7c8cccc06a2cdc993157598a44
ttinies <ttinies.v@gmail.com>
2018-09-20 21:34:07 -0600

    - added to enable quick/easy package testing

---
+++ sc2simulator/scenarioMgr/__main__.py
@@ -0,0 +1,13 @@
+
+"""Simple test of the scenarioMgr package"""
+
+import os
+
+from sc2simulator import constants as c
+from sc2simulator.scenarioMgr import parseBankXml
+
+
+if __name__=="__main__":
+    xmlfile = os.path.join(c.PATH_BANKS, "test.SC2Bank")
+    parseBankXml(xmlfile, debug=True)
+
