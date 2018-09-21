3d19bbc10df2274689b672b5f69384db2f389039
ttinies <ttinies.v@gmail.com>
2018-09-20 21:36:49 -0600

    - added uncommited, required file for constants definitions

---
+++ sc2simulator/constants.py
@@ -0,0 +1,11 @@
+
+import os
+
+from sc2common.constants import *
+from sc2common import types
+
+
+DEF_DURATION = 15
+PATH_NEW_MAPS = ""
+PATH_BANKS = os.path.join(USER_HOME_DIR, "Documents", "StarCraft II", "Banks")
+
