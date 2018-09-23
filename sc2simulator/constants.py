e4cb539d6f93a0e3908c478a5ab9d5ca045a8104
ttinies <ttinies.v@gmail.com>
2018-09-23 08:32:45 -0600

    - added three new constants: MAX_MAP_PICK_TRIES, MAX_UNIT_GRP_TRIES and MAX_TAG

--- sc2simulator/constants.py
+++ sc2simulator/constants.py
@@ -8,4 +8,7 @@ from sc2common import types
 DEF_DURATION = 15
 PATH_NEW_MAPS = ""
 PATH_BANKS = os.path.join(USER_HOME_DIR, "Documents", "StarCraft II", "Banks")
+MAX_MAP_PICK_TRIES = 30
+MAX_UNIT_GRP_TRIES = 10
+MAX_TAG = 65536 # 0x0000 - 0xFFFF
 
