3bd5e78394bc3b66f1f6adc98795a771de6512e2
ttinies <ttinies.v@gmail.com>
2018-10-02 09:17:49 -0600

    - implemented new function that can convert a sc2techTree unit into a ScenarioUnit: convertTechUnit() - a ScenarioUnit can now update it's attrs using the new update() method - a ScenarioUnit can now be hashed specifically by its tag, not by its memory address - a ScenarioUnit now differentiates itself based on whether it's a scenario unit or a 'base' unit (a base unit is one that appears in every scenario and grounds the two forces to their determined player locations.

--- sc2simulator/scenarioMgr/scenarioUnit.py
+++ sc2simulator/scenarioMgr/scenarioUnit.py
@@ -15,12 +15,16 @@ class ScenarioUnit(object):
         self.life       = 0.0
         self.shields    = 0.0
         self.xp         = 0
+        self.base       = False
     ############################################################################
     def __str__(self): return self.__repr__()
     def __repr__(self):
         attrList = ["%s=%s"%(k, v) for k, v in sorted(self.attrs.items())]
         attrStr = ", ".join(attrList)
         return "<%s %s>"%(self.__class__.__name__, attrStr)
+    ############################################################################
+    def __hash__(self):
+        return hash(self.tag)
     ############################################################################
     @property
     def attrs(self):
@@ -35,4 +39,22 @@ class ScenarioUnit(object):
     def unitType(self):
         """the tech tree definition for this specific unit"""
         return self.code
+    ############################################################################
+    def update(self, **attrs):
+        """update this unit's attributes"""
+        for k,v in attrs.items(): # assign attributes into unit
+            setattr(self, k, v)
+
+
+################################################################################
+def convertTechUnit(techUnit, **attrs):
+    """convert a tech unit into a scenario unit"""
+    u = ScenarioUnit(0)
+    u.update(nametype   = techUnit.name,
+             code       = techUnit.mType.code,
+             energy     = techUnit.energyStart,
+             life       = techUnit.healthMax,
+             shields    = techUnit.shieldsMax)
+    u.update(**attrs) # allow passed attributes to override techUnit defaults
+    return u
 
