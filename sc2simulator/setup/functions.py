1127c735b0349a8dd322f269747f06b35288a73d
ttinies <ttinies.v@gmail.com>
2018-10-07 19:23:24 -0600

    - sc2maptool dimensions can now be used for both generated and randonly generated custom scenarios - bug fix: specified cases must be presented as a list from a string separated by various allowed delimiters

--- sc2simulator/setup/functions.py
+++ sc2simulator/setup/functions.py
@@ -4,6 +4,7 @@ Fully define a 'random' generated setup for the players per specified conditions
 """
 
 import random
+import re
 
 from sc2simulator import constants as c
 from sc2simulator.scenarioMgr import Scenario, parseBankXml, getBankFilepath
@@ -16,34 +17,12 @@ from sc2simulator.setup.unitSelection import generatePlayerUnits
 ################################################################################
 def getSetup(mapObj, options, cfg):
     """get the appropriate scenarios, whether predefined or custom generated"""
-    scenarios = []
-    if options.cases: # use preloaded cases
-        try:
-            bankName = getBankFilepath(mapObj.name) # select the bank repository from the specified map
-            bank = parseBankXml(bankName) # load the bank's defined scenarios
-        except:
-            print("ERROR: failed to load specified bank '%s'"%(mapObj.name))
-            return scenarios
-        for scenarioName in options.cases: # use options.cases to select which cases to run
-            try: # attempt to retrieve the specified scenario from the bank
-                scenarios.append(bank.scenarios[scenarioName])
-            except:
-                print("WARNING: scenario '%s' was not found in bank %s"%(
-                    scenarioName, bankName))
-    else: # dynamically generate the scenario
-        scenarios += generateScenario(mapObj, options, cfg)
-    return scenarios
-
-
-################################################################################
-def generateScenario(mapObj, options, cfg):
-    """override this function is different generation methods are desired"""
-    hg = None
+    mdata = None
     try:
         import sc2maps # Versentiedge closed source package
         mData = sc2maps.MapData(mapName=mapObj.name)
         mapLocations.mapDimensions = mData.dimensions.toCoords()
-        hg = mData.placement.halfGrid
+        mdata = mData.placement.halfGrid
     except Exception: # ModuleNotFoundError isn't available in python 3.5
         try:
             if not options.dimensions:  raise ValueError("must provide dims")
@@ -55,7 +34,29 @@ def generateScenario(mapObj, options, cfg):
             print("ERROR: must provide valid map dimensions (given: %s"%(
                 options.dimensions))
             return []
-        hg = mapLocations.mapDimensions
+        mdata = mapLocations.mapDimensions
+    scenarios = []
+    if options.cases: # use preloaded cases
+        try:
+            bankName = getBankFilepath(mapObj.name) # select the bank repository from the specified map
+            bank = parseBankXml(bankName) # load the bank's defined scenarios
+        except:
+            print("ERROR: failed to load specified bank '%s'"%(mapObj.name))
+            return scenarios
+        for scenarioName in re.split("[,;:]+", options.cases): # use options.cases to select which cases to run
+            try: # attempt to retrieve the specified scenario from the bank
+                scenarios.append(bank.scenarios[scenarioName])
+            except:
+                print("WARNING: scenario '%s' was not found in bank %s"%(
+                    scenarioName, bankName))
+    else: # dynamically generate the scenario
+        scenarios += generateScenario(mapObj, options, cfg, mdata)
+    return scenarios
+
+
+################################################################################
+def generateScenario(mapObj, options, cfg, mdata):
+    """override this function is different generation methods are desired"""
     scenario = Scenario("custom%s"%mapObj.name)
     scenario.duration = options.duration
     d = options.distance # generate each player's locations
@@ -65,7 +66,7 @@ def generateScenario(mapObj, options, cfg):
         race = pickRace(r)
         pIdx = i + 1 # player indexes start at one, not zero
         scenario.addPlayer(pIdx, loc=pLoc, race=race)
-        generatePlayerUnits(scenario, pIdx, race, options, pLoc, mapData=hg)
+        generatePlayerUnits(scenario, pIdx, race, options, pLoc, mapData=mdata)
         try:    generateUpgrades(scenario, pIdx, options)
         except Exception as e:
             print("ERROR: %s"%e)
