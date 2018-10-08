0d2355524b80a6fa1d6542d5ac74163596d95612
ttinies <ttinies.v@gmail.com>
2018-10-07 19:44:24 -0600

    - version bump

--- sc2simulator/__version__.py
+++ sc2simulator/__version__.py
@@ -17,6 +17,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = (0, 6, 2)
+VERSION = (0, 7, 0)
 
 __version__ = '.'.join(map(str, VERSION))
