3f5ec96ac12b9a353016dd26dc5afd73ad13737f
ttinies <ttinies.v@gmail.com>
2018-10-07 19:21:41 -0600

    - added method initPlayers() which accounts for various scenario requirements which aren't presented in the scenario bank XML definition, such as each player's race and starting camera position and each unit's in-game code

--- sc2simulator/scenarioMgr/bank.py
+++ sc2simulator/scenarioMgr/bank.py
@@ -1,4 +1,7 @@
 
+from sc2simulator import constants as c
+from sc2simulator.setup.simpleRaceMap import codeMap
+
 
 ################################################################################
 class Bank(object):
@@ -32,4 +35,51 @@ class Bank(object):
             raise KeyError("cannot add '%s' because %s is already defined"%(
                 scenario.name, scenario))
         self.scenarios[scenario.name] = scenario
+    ############################################################################
+    def initPlayers(self):
+        """all players must define their initial starting positions and races"""
+        ########################################################################
+        def findUnitRace(unit):
+            """locate the race in predefined data of unit given its name"""
+            name = unit.nametype
+            for race, data in codeMap.items():
+                for uType, codes in data.items():
+                    for code, target in codes.items():
+                        if name != target:  continue
+                        unit.code = code # also update the unit's code type
+                        return race
+            print("WARNING: failed to locate the race of unit '%s'."%name)
+            return None
+        ########################################################################
+        unitsToIgnore = { # ignore these units which are saved in the XML, but not placed
+            "BroodlingEscort",
+            "Broodling",
+        }
+        for s in self.scenarios.values():
+            for u in list(s.units.values()):
+                if u.nametype in unitsToIgnore:
+                      del s.units[u.tag] # unit is ignored
+                else: u.tag = 0 # unit tag is reset for general use
+            for p in s.players.values():
+                if p.position == None: # player's location is not already defined
+                    units = p.units
+                    x, y = (0, 0)
+                    for u in units:
+                        uX, uY = u.position[:2]
+                        x += uX
+                        y += uY
+                    numUnits = len(units)
+                    p.position = (x / numUnits, y / numUnits) # average position is calculated as the player's starting location
+                if p.race == None: # pick the appropriate race for this player
+                    allowed = list(c.types.ActualRaces.ALLOWED_TYPES)
+                    try:    allowed.remove(None)
+                    except: pass
+                    allowed = dict(zip(allowed, [0]*len(allowed))) # unit counter is initialized
+                    for u in p.units:
+                        race = findUnitRace(u)
+                        if race:
+                            allowed[race] += 1
+                    count, p.race = max([
+                        (count, r) for r, count in allowed.items()
+                    ])
 
